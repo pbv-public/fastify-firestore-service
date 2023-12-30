c581134d3ecd5536d4996f65b32a9c311417adf2
pbv <dev@pb.vision>
2023-12-30 12:53:00 -0800

    use git hash that triggered the build
    the git hash in the working folder may be different

--- docs/build.sh
+++ docs/build.sh
@@ -14,7 +14,11 @@ npx jsdoc --configure jsdoc.config.json ../src/plugins `find ../src -type f -not
 sed -e "s/docs\\/api[.]md/libs\\/api\\/index.html/g" generated/index.html > generated/tmp
 mv generated/tmp generated/index.html
 
-gitHash=`git rev-parse HEAD`
+if [ "$GITHUB_SHA" = "" ]; then
+    gitHash=`git rev-parse HEAD`
+else
+    gitHash=$GITHUB_SHA
+fi
 newLine="Generated from <a href=\"$gitHash\">$gitHash<\\/a><\\/article>"
 cat ./generated/index.html | sed -e "s/[<][/]article[>]/$newLine/g" > tmp
 mv tmp ./generated/index.html
