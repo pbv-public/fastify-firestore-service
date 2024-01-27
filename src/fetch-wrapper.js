f69971780afe620beaa67556af4015f868de691d
pbv <dev@pb.vision>
2024-01-27 12:03:00 -0800

    rename fetchWrapper options param to request
    differentiate clearly from the options param we'll pass to node-fetch
    which is a completely different thing

--- src/fetch-wrapper.js
+++ src/fetch-wrapper.js
@@ -3,23 +3,24 @@ import zlib from 'node:zlib'
 
 import realFetch from 'node-fetch'
 
-async function fetchWrapper (options, mockedFetch) {
-  const { compress = true, method = 'POST', url, qsParams, json } = options
-  let { body, headers } = options
+async function fetchWrapper (request, mockedFetch) {
+  const { compress = true, method = 'POST', url, qsParams, json } = request
+  let { body, headers } = request
   headers = { ...headers } // copy the headers before we change them
 
   // compress the body using brotli
   if (json) {
     headers['content-type'] = 'application/json'
-    body = JSON.stringify(options.json)
+    body = JSON.stringify(request.json)
   }
   if (body && compress) {
     body = zlib.brotliCompressSync(body)
     headers['content-encoding'] = 'br'
   }
 
-  // istanbul ignore next
-  const fetch = mockedFetch ?? fetchWrapper.__mock ?? realFetch
+  const fetch = (mockedFetch === false)
+    ? realFetch
+    : (mockedFetch ?? fetchWrapper.__mock ?? realFetch)
 
   // compute the full URL including search params
   let fullURL = url
@@ -29,7 +30,8 @@ async function fetchWrapper (options, mockedFetch) {
       fullURL += `?${qsStr}`
     }
   }
-  return fetch(fullURL, { body, headers, method, compress: false })
+  const options = { body, headers, method, compress: false }
+  return fetch(fullURL, options)
 }
 
 export default fetchWrapper
